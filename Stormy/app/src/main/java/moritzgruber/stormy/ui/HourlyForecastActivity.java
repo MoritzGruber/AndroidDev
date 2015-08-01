df913fe0a4ee28f5dd72f7e2c799f07f00b3bd31
Moritz Gruber <mog8017@thi.de>
2015-08-01 15:03:30 +0200

    wetter fertig
    fertig mit wetter app tut

---
+++ Stormy/app/src/main/java/moritzgruber/stormy/ui/HourlyForecastActivity.java
@@ -0,0 +1,55 @@
+package moritzgruber.stormy.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import moritzgruber.stormy.R;
+import moritzgruber.stormy.adapters.HourAdapter;
+import moritzgruber.stormy.wether.Hour;
+
+import java.util.Arrays;
+
+public class HourlyForecastActivity extends ActionBarActivity {
+
+    private Hour[] mHours;
+
+    @Bind(R.id.reyclerView) RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+        mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        HourAdapter adapter = new HourAdapter(this, mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
